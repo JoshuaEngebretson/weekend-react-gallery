71a1c7763beb1e8b02258031042cf9642cd7438b
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-28 16:48:58 -0500

    Wrote descriptions, paths, and provided likes for each image
    - This was written in the gellery.data.js file inside of the modules folder, within the server folder.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,40 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {
+        id: 1,
+        path: 'images/goat_small.jpg',
+        description: 'Photo of a goat taken at Glacier National Park.',
+        likes: 1
+    },
+    {
+        id: 2,
+        path: 'images/Howls_Moving_Castle.jpg',
+        description: 'Movie poster for the Studio Ghibli film "Howls Moving Castle"',
+        likes: 3
+    },
+    {
+        id: 3,
+        path: 'images/Nausicaa_of_the_Valley_of_the_Wind.jpeg',
+        description: 'Movie poster for the Studio Ghibli film "Nausicaa of the Valley of the Wind"',
+        likes: 0
+    },
+    {
+        id: 4,
+        path: 'images/Ponyo.jpeg',
+        description: 'Movie poster for the Studio Ghibli film "Ponyo"',
+        likes: 0
+    },
+    {
+        id: 5,
+        path: 'images/Princess_Mononoke.jpeg',
+        description: 'Movie poster for the Studio Ghibli film "Princess Mononoke"',
+        likes: 2
+    },
+    {
+        id: 6,
+        path: 'images/Spirited_Away.jpeg',
+        description: 'Movie poster for the Studio Ghibli film "Spirited Away"',
+        likes: 4
+    }
 ];
 
 module.exports = galleryItems;
