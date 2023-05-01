4665cede9bc37d3ff28cbc767717d8edf3e7d80d
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-05-01 09:29:00 -0500

    Added comments to GalleryItem.jsx

--- src/components/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem/GalleryItem.jsx
@@ -9,25 +9,38 @@ const GalleryItem = (props) => {
   
   const item = props.item;
 
+  // Function to check if showText is true or false,
+  //  Conditionally renders either;
+  //    the description 
+  //    OR
+  //    the image based on that state
   const ImageOrText = () => {
+    // If text showText is true, render the text
     if (showText){
       return <p className='square text'>{item.description}</p>
     }
+    // Else, render the image
     else {
       return <img src={item.path} className='square'/>
     }
-  }
+  } // End ImageOrText
 
+  // Function to change showText from true > false
+  //  and from false > true
   const toggleText = () => {
+    // If showText is currently true, toggle to false
     if (showText) {
       setShowText(false)
     }
+    // Else, toggle to true
     else {
       setShowText(true)
     }
-  }
+  } // End toggleText
 
+  // Function to addLike to the item where the data is stored
   const addLike = () => {
+    // Send PUT request to /gallery/like/:id
     axios({
       method: 'PUT',
       url: `/gallery/like/${item.id}`
@@ -39,11 +52,14 @@ const GalleryItem = (props) => {
       Swal.fire({
         text: `There was an error updating the like status for the image of ${item.title}.`
       })
+      // and log that error
       console.log('Error:', err);
     })
-  }
+  } // End addLike
 
+  // Function to delete the item where the data is stored
   const deleteItem = () => {
+    // Pop up alert to confirm if the user wants to delete the item
     Swal.fire({
       icon: 'warning',
       title: 'Delete Confirmation',
@@ -53,36 +69,42 @@ const GalleryItem = (props) => {
       cancelButtonText: 'No, I want to cancel!',
       reverseButtons: true
     }).then((result) => {
+      // If confirmed, delete the item and alert the user about success or failure
       if(result.isConfirmed){
-          axios({
-            method: 'DELETE',
-            url: `/gallery/${item.id}`
-          }).then(res => {
-            Swal.fire({
-              icon: 'success',
-              title: 'Delete Complete',
-              text: `${item.title} has been deleted from the database.`
-            })
-            props.getGalleryList();
-          }).catch(err => {
-            Swal.fire({
-              icon: 'error',
-              text: `An error occured while deleting ${item.title}, please try again later.`
-            })
-            console.log('Delete Error:', err);
+        axios({
+          method: 'DELETE',
+          url: `/gallery/${item.id}`
+        }).then(res => {
+          // If successful, alert the user
+          Swal.fire({
+            icon: 'success',
+            title: 'Delete Complete',
+            text: `${item.title} has been deleted from the database.`
           })
+          // and pull the Gallery List from the database again
+          props.getGalleryList();
+        }).catch(err => {
+          // If unsuccessful, alert the user there was an error
+          Swal.fire({
+            icon: 'error',
+            text: `An error occured while deleting ${item.title}, please try again later.`
+          })
+          // and log the error
+          console.log('Delete Error:', err);
+        })
       }
       else if (result.dismiss === Swal.DismissReason.cancel){
         // If cancelled, alert the user that this item was not deleted
         Swal.fire({
-            icon: 'error',
-            title: 'Phew!',
-            text: `That was close, ${item.title} is still in the database`
+          icon: 'error',
+          title: 'Phew!',
+          text: `That was close, ${item.title} is still in the database`
         })
       }
     })
-  }
+  } // End deleteItem
 
+  // 👇 Is what a GalleryItem looks like
   return (
     <div className='item-card'>
       <div className='text-image' onClick={toggleText}>
